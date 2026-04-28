fb0612460bf462e4498e983fb3313bb8bbaef7c1
i4444i4444 <sharagmasha@gmail.com>
2026-04-28 21:22:55 +0300

    feat: implement reverse int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
-};
+function reverse(n) {
+  return parseInt(Math.abs(n).toString().split('').reverse().join(''), 10);
+}
+
+module.exports = reverse;
